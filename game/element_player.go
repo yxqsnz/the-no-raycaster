6a800c3497d9dc9ff8f40718738f2de79ba7eda6
yxqsnz <yxqsnz@gmail.com>
2023-01-09 11:38:21 -0300

    feat: funny mode

--- game/element_player.go
+++ game/element_player.go
@@ -4,5 +4,13 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 func RenderPlayerInMap(cx *Context) {
 	rect_h, rect_w := cx.GetRectSize()
-	Retangle(int(cx.Player.X*float64(rect_w))/cx.MapDeScale, int(cx.Player.Y*float64(rect_h))/cx.MapDeScale, 5, 5, rl.White)
+	render := func() {
+		Retangle(int(cx.Player.X*float64(rect_w))/cx.MapDeScale, int(cx.Player.Y*float64(rect_h))/cx.MapDeScale, 5, 5, rl.White)
+	}
+
+	if cx.FunnyMode {
+		go render()
+	} else {
+		render()
+	}
 }
--- game/element_rays.go
+++ game/element_rays.go
@@ -6,6 +6,14 @@ func RenderRays(cx *Context) {
 	rect_h, rect_w := cx.GetRectSize()
 
 	for _, ray := range cx.Rays {
-		rl.DrawRectangle(int32(ray.X*float64(rect_w)/float64(cx.MapDeScale)), int32(ray.Y*float64(rect_h)/float64(cx.MapDeScale)), 1, 1, rl.Pink)
+		render := func() {
+			rl.DrawRectangle(int32(ray.X*float64(rect_w)/float64(cx.MapDeScale)), int32(ray.Y*float64(rect_h)/float64(cx.MapDeScale)), 1, 1, rl.Pink)
+		}
+
+		if cx.FunnyMode {
+			go render()
+		} else {
+			render()
+		}
 	}
 }
